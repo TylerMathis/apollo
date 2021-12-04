c73e27352eac73ed3151a808d7fdcd5ca369118b
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-12-03 23:27:17 -0500

    Run specified checker

--- apollo.py
+++ apollo.py
@@ -1,6 +1,15 @@
-import arguments, diff_checker, token_checker, epsilon_checker
+import arguments, diff_checker, token_checker, epsilon_checker, sys
 
 args = arguments.parse()
 
 with open(args.user_output) as user_out, open(args.judge_output) as judge_out:
-    print(epsilon_checker.check(user_out, judge_out, 1e-6).to_string())
+    if args.tok:
+        res = token_checker.check(user_out, judge_out)
+    elif args.eps:
+        res = epsilon_checker.check(user_out, judge_out, args.eps)
+    else:
+        res = diff_checker.check(user_out, judge_out)
+
+    print(res.to_string())
+    sys.exit(res.verdict.short_name != 'AC')
+
