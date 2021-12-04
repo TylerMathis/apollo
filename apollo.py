733b073277bffa03e4bd178ede39b0c2b34f50e2
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-12-03 22:29:55 -0500

    Epsilon Checker

--- apollo.py
+++ apollo.py
@@ -1,6 +1,6 @@
-import arguments, diff_checker, token_checker
+import arguments, diff_checker, token_checker, epsilon_checker
 
 args = arguments.parse()
 
 with open(args.user_output) as user_out, open(args.judge_output) as judge_out:
-    print(token_checker.check(user_out, judge_out).to_string())
+    print(epsilon_checker.check(user_out, judge_out, 1e-6).to_string())
