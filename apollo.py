86b647730dee6d9994e7965b20a2d6d5f1c31459
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-12-03 22:08:36 -0500

    Token Checker

--- apollo.py
+++ apollo.py
@@ -1,6 +1,6 @@
-import arguments, diff_checker
+import arguments, diff_checker, token_checker
 
 args = arguments.parse()
 
 with open(args.user_output) as user_out, open(args.judge_output) as judge_out:
-    print(diff_checker.check(user_out, judge_out).to_string())
+    print(token_checker.check(user_out, judge_out).to_string())
